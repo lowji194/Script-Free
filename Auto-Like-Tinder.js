ee91f5722ddbd503866e51e5fe7628ff70295735
lowji194 <theloi194@gmail.com>
2024-10-12 08:02:52 +0700

    Update Auto-Like-Tinder.js

--- Auto-Like-Tinder.js
+++ Auto-Like-Tinder.js
@@ -1,6 +1,6 @@
-setTimeout(function loop() {
+javascript: setTimeout(function loop() {
     try {
-document.getElementsByClassName('button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style gamepad-button Bxsh($bxsh-btn) Expand Ov(h) Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a')[0].click();
-        catch {}
+        document.getElementsByClassName('button Lts($ls-s) Z(0) CenterAlign Mx(a) Cur(p) Tt(u) Bdrs(50%) P(0) Fw($semibold) focus-button-style gamepad-button Bxsh($bxsh-btn) Expand Ov(h) Trstf(e) Trsdu($normal) Wc($transform) Pe(a) Scale(1.1):h Scale(.9):a Bgi($g-ds-background-like):a')[0].click();
+    } catch (e) {}
     setTimeout(loop, 3000);
-}, 1000)
+}, 1000);
